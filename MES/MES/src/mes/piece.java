3e10d45f2cf4d01e5c3783d6dcac1e6695378438
Vasco Costa <vascom.c@hotmail.com>
2022-05-15 23:53:06 +0100

    Small changes

--- MES/MES/src/mes/piece.java
+++ MES/MES/src/mes/piece.java
@@ -6,6 +6,7 @@ public class piece {
 	static int[] machines; // tem em cada posição a próxima máquinas que vai utilizar
 	static int priority;
 	static int final_form;
+	static int curr_form;
 	
 	public int getOrderid() {
 		return orderid;
@@ -27,6 +28,12 @@ public class piece {
 	}
 	public int getFinal_form() {
 		return final_form;
+	}
+	public static int getCurr_form() {
+		return curr_form;
+	}
+	public static void setCurr_form(int curr_form) {
+		piece.curr_form = curr_form;
 	}
 	public void setFinal_form(int final_form) {
 		this.final_form = final_form;
--- MES/MES/src/mes/sorter.java
+++ MES/MES/src/mes/sorter.java
@@ -4,126 +4,148 @@ import java.util.ArrayList;
 
 public class sorter {
 	
-	
+	public static ArrayList<piece> day_pieces;
 	public static void main(String[] args) {
 		
-		ArrayList<piece> day_pieces=database.getpieces();
-		decide_mach()
-	}
+		
+		while (1) {
+			read_vars();
+			
+			if(warning_start_day==1) {
+				day_pieces=database.getpieces();
+				decide_mach(null, day_pieces);
+				update_results();
+			}
+			
+			if(warning_mid_day==1) {
+				decide_tools(null, day_pieces);
+			}
+			
+		}
+		
 	
 }
 
+private static void decide_tools(Object object, ArrayList<piece> day_pieces2) {
+				
+	}
+
 public int decide_mach(int[] tools, ArrayList<piece> day_pieces) {
 	
-	for(int i=0;i<day_pieces.machines.length;i++) p.machines[i]=0;		
-	
-	//PIECE 3
-	if(p.final_form==3) 
-	{
-		for (int i =1; i<6; i++) 
-		{
-			if(tools[i]==2) {
-				p.machines[0]=i;
-				break;
-			}
-		}
+	for(int j=0;j<day_pieces.length;j++) {
+		for(int i=0;i<day_pieces.machines.length;i++) day_pieces[j].machines.[i]=0;		
 	}
 	
-	//PIECE 4
-	else if(p.final_form==4) 
-	{
-		for (int i =1; i<6; i++) 
+	for(int j=0;j<day_pieces.length;j++) {
+		//PIECE 3
+		if(p.final_form==3) 
 		{
-			if(tools[i]==3) {
-				p.machines[0]=i;
-				break;
-			}
-		}
-	}
-	
-	//PIECE 5
-	else if(p.final_form==5) 
-	{
-		for (int i =1; i<6; i++) 
-		{
-			if(tools[i]==4) {
-				p.machines[0]=i;
-				break;
-			}
-		}
-	}
-	
-	//PIECE 6
-	else if(p.final_form==6) 
-	{	
-		if(warehouse.p1>0) {
-			for (int i =1; i<6; i++) 
-			{
-				if(tools[i]==1) {
-					p.machines[0]=i;
-					break;
-				}
-			}
-		}
-		else {
 			for (int i =1; i<6; i++) 
 			{
 				if(tools[i]==2) {
-					p.machines[0]=i;
+					day_pieces[j].machines[0]=i;
+					break;
 				}
-				else if(tools[i]==1 && p.machines[0]!=0) {
-					p.machines[1]=i;
+			}
+		}
+		
+		//PIECE 4
+		else if(p.final_form==4) 
+		{
+			for (int i =1; i<6; i++) 
+			{
+				if(tools[i]==3) {
+					day_pieces[j].machines[0]=i;
+					break;
+				}
+			}
+		}
+		
+		//PIECE 5
+		else if(p.final_form==5) 
+		{
+			for (int i =1; i<6; i++) 
+			{
+				if(tools[i]==4) {
+					day_pieces[j].machines[0]=i;
+					break;
+				}
+			}
+		}
+		
+		//PIECE 6
+		else if(p.final_form==6) 
+		{	
+			if(warehouse.p1>0) {
+				for (int i =1; i<6; i++) 
+				{
+					if(tools[i]==1) {
+						day_pieces[j].machines[0]=i;
+						break;
+					}
+				}
+			}
+			else {
+				for (int i =1; i<6; i++) 
+				{
+					if(tools[i]==2) {
+						day_pieces[j].machines[0]=i;
+					}
+					else if(tools[i]==1 && day_pieces[j].machines[0]!=0) {
+						day_pieces[j].machines[1]=i;
+						break;
+					}
+				}
+			}
+		}
+			
+		//PIECE 7
+		else if(p.final_form==7) 
+		{
+			for (int i =1; i<6; i++) 
+			{
+				if(tools[i]==3) {
+					day_pieces[j].machines[0]=i;
+				}
+				else if(tools[i]==4 && day_pieces[j].machines[0]!=0) {
+					day_pieces[j].machines[1]=i;
+					break;
+				}
+			}
+		}	
+		
+		//PIECE 8
+		else if(p.final_form==8) 
+		{
+			for (int i =1; i<6; i++) 
+			{
+				if(tools[i]==1) {
+					day_pieces[j].machines[0]=i;
+				}
+				else if(tools[i]==3 && day_pieces[j].machines[0]!=0) {
+					day_pieces[j].machines[1]=i;
+					break;
+				}
+			}
+		}	
+		
+		//PIECE 9
+		else if(p.final_form==9) 
+		{
+			for (int i =1; i<6; i++) 
+			{
+				if(tools[i]==3) {
+					day_pieces[j].machines[0]=i;
+				}
+				else if(tools[i]==4 && day_pieces[j].machines[0]!=0) {
+					day_pieces[j].machines[1]=i;
+				}
+				else if(tools[i]==3 && day_pieces[j].machines[1]!=0) {
+					day_pieces[j].machines[2]=i;
 					break;
 				}
 			}
 		}
 	}
-		
-	//PIECE 7
-	else if(p.final_form==7) 
-	{
-		for (int i =1; i<6; i++) 
-		{
-			if(tools[i]==3) {
-				p.machines[0]=i;
-			}
-			else if(tools[i]==4 && p.machines[0]!=0) {
-				p.machines[1]=i;
-				break;
-			}
-		}
-	}	
 	
-	//PIECE 8
-	else if(p.final_form==8) 
-	{
-		for (int i =1; i<6; i++) 
-		{
-			if(tools[i]==1) {
-				p.machines[0]=i;
-			}
-			else if(tools[i]==3 && p.machines[0]!=0) {
-				p.machines[1]=i;
-				break;
-			}
-		}
-	}	
-	
-	//PIECE 9
-	else if(p.final_form==9) 
-	{
-		for (int i =1; i<6; i++) 
-		{
-			if(tools[i]==3) {
-				p.machines[0]=i;
-			}
-			else if(tools[i]==4 && p.machines[0]!=0) {
-				p.machines[1]=i;
-			}
-			else if(tools[i]==3 && p.machines[1]!=0) {
-				p.machines[2]=i;
-				break;
-			}
-		}
-	}
 }
