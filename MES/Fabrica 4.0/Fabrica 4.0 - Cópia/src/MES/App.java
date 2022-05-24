7e7286539eb23183023886d6ab2cde00fc56dd56
Vasco Costa <vascom.c@hotmail.com>
2022-05-24 12:20:42 +0100

    WRITE OPC UA ARRAY

--- MES/Fabrica 4.0/Fabrica 4.0 - Cópia/src/MES/App.java
+++ MES/Fabrica 4.0/Fabrica 4.0 - Cópia/src/MES/App.java
@@ -18,20 +18,20 @@ public class App {
         client.connect().get();
         
         
-        NodeId nodeId = new NodeId(4,"|var|CODESYS Control Win V3 x64.Application.Lista_Vars.E2_RN_S");
-        String Variable = client.readValue(0, TimestampsToReturn.Both, nodeId).get().toString().substring(29, 31);
+        NodeId nodeId = new NodeId(4,"|var|CODESYS Control Win V3 x64.Application.Lista_Vars.C1");
+        String Variable = client.readValue(0, TimestampsToReturn.Both, nodeId).get().toString().substring(30, 49);
         System.out.println(Variable);
         
         
-        NodeId writeId = new NodeId(4,"|var|CODESYS Control Win V3 x64.Application.Lista_Vars.E2_RN_S");
-        short i=2;
+        NodeId writeId = new NodeId(4,"|var|CODESYS Control Win V3 x64.Application.Lista_Vars.C1");
+        short[] i= {-1,0,0,0,0};
         Variant v = new Variant( i);
         
         DataValue dv = new DataValue(v, null, null);
         Variable = client.writeValue(writeId, dv).get().toString();
         System.out.println(Variable);
         
-        Variable = client.readValue(0, TimestampsToReturn.Both, nodeId).get().toString().substring(29, 31);
+        Variable = client.readValue(0, TimestampsToReturn.Both, nodeId).get().toString().substring(30, 49);
         System.out.println(Variable);
         
 //        NodeId nodeId = new NodeId(4,"|var|CODESYS Control Win V3 x64.Application.Lista_Vars.C1");
